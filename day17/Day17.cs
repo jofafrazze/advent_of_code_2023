f197f843f0e6d8e8663cf59028ae966aa6db5552
jofafrazze <johan@hallonbrax.com>
2023-12-17 12:29:43 +0100

    Day 17 condensed

--- day17/Day17.cs
+++ day17/Day17.cs
@@ -6,79 +6,36 @@ namespace aoc
 {
     public class Day17
     {
-        // Today: 
-        record struct Step(Pos p, Pos dir, int nStraight);
-        static int WalkMap(Map m, List<Step> start)
+        // Clumsy Crucible: Walk 2D map, find minimum sum to reach target position
+        record struct Step(Pos pos, Pos dir, int len);
+        static int WalkMap(Map m, List<Step> start, bool part1)
         {
             HashSet<Step> visited = new();
-            var toVisit = new PriorityQueue<Step, int>();
-            foreach (var s in start)
-                toVisit.Enqueue(s, 0);
+            var toVisit = new PriorityQueue<Step, int>(start.Select(w => (w, 0)));
             Pos endPos = new(m.width - 1, m.height - 1);
+            int maxLen = part1 ? 3 : 10;
             while (toVisit.TryDequeue(out Step cur, out int curScore))
             {
-                if (cur.p == endPos)
+                if (cur.pos == endPos && (part1 || cur.len >= 3))
                     return curScore;
                 if (visited.Contains(cur))
                     continue;
                 visited.Add(cur);
                 foreach (Pos dir in CoordsRC.directions4)
                 {
-                    Step next = new(cur.p + cur.dir, dir, (dir == cur.dir) ? cur.nStraight + 1 : 0);
-                    if (dir != -cur.dir && m.HasPosition(next.p) && next.nStraight < 3)
-                        toVisit.Enqueue(next, curScore + m[next.p] - '0');
+                    Step next = new(cur.pos + cur.dir, dir, (dir == cur.dir) ? cur.len + 1 : 0);
+                    if (dir != -cur.dir && m.HasPosition(next.pos) && next.len < maxLen)
+                        if (part1 || (dir == cur.dir || cur.len >= 3))
+                            toVisit.Enqueue(next, curScore + m[next.pos] - '0');
                 }
             }
             throw new Exception("Not found!");
         }
-        static int WalkMap2(Map m, List<Step> start)
-        {
-            Dictionary<Step, int> visited = new();
-            var toVisit = new PriorityQueue<Step, int>();
-            foreach (var s in start)
-            {
-                visited[s] = 0;
-                toVisit.Enqueue(s, 0);
-            }
-            int bestSum = int.MaxValue;
-            Pos bottomRight = new Pos(m.width - 1, m.height - 1);
-            while (toVisit.TryDequeue(out Step cur, out int score))
-            {
-                foreach (Pos dir in CoordsRC.directions4)
-                {
-                    if (dir != -cur.dir)
-                    {
-                        Step next = new(cur.p + cur.dir, dir, (dir == cur.dir) ? cur.nStraight + 1 : 0);
-                        if ((dir == cur.dir) || cur.nStraight >= 3)
-                        {
-                            if (m.HasPosition(next.p) && next.nStraight < 10)
-                            {
-                                int nextSum = visited[cur] + (m[next.p] - '0');
-                                if (nextSum < bestSum)
-                                {
-                                    if (!visited.ContainsKey(next) || visited[next] > nextSum)
-                                    {
-                                        visited[next] = nextSum;
-                                        toVisit.Enqueue(next, nextSum);
-                                        if (next.p == bottomRight && next.nStraight >= 4)
-                                            bestSum = nextSum;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            return bestSum;
-        }
         public static (Object a, Object b) DoPuzzle(string file)
         {
-            var input = ReadInput.Strings(Day, file);
-            Map m = Map.Build(input);
+            Map m = Map.Build(ReadInput.Strings(Day, file));
             List<Step> steps = new() { new Step(new Pos(), CoordsRC.right, 0), new Step(new Pos(), CoordsRC.down, 0) };
-            int a = WalkMap(m, steps);
-            int b = WalkMap2(m, steps);
-            return (a, b);
+            return (WalkMap(m, steps, true), WalkMap(m, steps, false));
         }
         static void Main() => Aoc.Execute(Day, DoPuzzle);
         static string Day => Aoc.Day(MethodBase.GetCurrentMethod()!);
